c2553d5ec38b1e0f3dac345f2095c46a94ccae64
Amandeep8848 <eight848-digital@DESKTOP-ARCF17F>
2024-06-07 16:44:00 +0530

    fix: updated assigning role profile code

--- keycloak/api/v1/assign_role_profile.py
+++ keycloak/api/v1/assign_role_profile.py
@@ -3,13 +3,18 @@ from frappe import _
 
 @frappe.whitelist()
 def assign_role_profile_in_frappe(kwargs):
-	try:
-		erp_username = frappe.db.get_value("Erpnext Keycloak User Mapping",{"keycloak_id": kwargs["user_id"]},"erpnext_username")
-		if erp_username is not None:
-			doc = frappe.get_doc("User",erp_username)
-			doc.role_profile_name = kwargs["role_details"][0]["name"] if kwargs["operation"] == "assign" else None
-			doc.save(ignore_permissions=True)
-		else:
-			frappe.log_error("Username not found")
-	except Exception as e:
-		frappe.log_error("Issue : ", e)
+    try:
+        erp_username = frappe.db.get_value("Erpnext Keycloak User Mapping", {"keycloak_id": kwargs["user_id"]}, "erpnext_username")
+        if erp_username is not None:
+            doc = frappe.get_doc("User", erp_username)
+            if kwargs["operation"] == "assign":
+                doc.role_profile_name = kwargs["role_details"][0]["name"]
+            elif kwargs["operation"] == "unassign":
+                doc.role_profile_name = None
+                if len(doc.roles) > 0:
+                    doc.roles.clear()
+            doc.save(ignore_permissions=True)
+        else:
+            frappe.log_error("Username not found")
+    except Exception as e:
+        frappe.log_error("Issue: " + str(e))
