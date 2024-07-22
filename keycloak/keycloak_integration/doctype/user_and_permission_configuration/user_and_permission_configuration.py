0944d5e93cc0f910e6ce3c0eab6dcad36310f577
Amandeep8848 <eight848-digital@DESKTOP-ARCF17F>
2024-07-22 13:56:10 +0530

    fix: filters and validations

--- keycloak/keycloak_integration/doctype/user_and_permission_configuration/user_and_permission_configuration.py
+++ keycloak/keycloak_integration/doctype/user_and_permission_configuration/user_and_permission_configuration.py
@@ -1,8 +1,26 @@
 # Copyright (c) 2024, Amandeep and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class UserandPermissionConfiguration(Document):
 	pass
+
+@frappe.whitelist()
+def filter_doctypes_based_on_permissions(doctype, txt, searchfield, start, page_len, filters):
+	print("1")
+	permission_type = filters.get("permission_type")
+	print(permission_type)
+	if permission_type:
+		filtered_doctypes = f"""
+			SELECT child.allow_doctype
+			FROM `tabPermission Type Doctype` as child
+			JOIN `tabPermission Type` as parent
+			ON child.parent = parent
+			WHERE parent.name1 = "{permission_type}"
+		"""
+		print(frappe.db.sql(filtered_doctypes))
+		return frappe.db.sql(filtered_doctypes)
+	else:
+		return []
