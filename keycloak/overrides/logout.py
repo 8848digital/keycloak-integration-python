2b677107dd22185c2abed12da390e3c46e121f20
Touhid Mullick <touhid@8848digital.com>
2024-09-25 20:31:41 +0530

    fix: sso logout

--- keycloak/overrides/logout.py
+++ keycloak/overrides/logout.py
@@ -2,30 +2,34 @@ import frappe
 import requests
 import json
 import urllib.parse
+from keycloak.utils.utils import get_access_token
 
 def logout():
     provider = frappe.request.cookies.get('provider')
-    if provider:
+    if provider == "keycloak":
         logout_user_from_provider(provider)
 
 def logout_user_from_provider(provider):
-    provider_details = frappe.get_doc("Social Login Key", provider)
-    if provider_details.logout_url:
-        client_secret = provider_details.get_password("client_secret")
-        access_token_details = json.loads(urllib.parse.unquote(frappe.request.cookies.get(provider)))
-        if (provider_details.base_url
-            and provider_details.logout_url
-            and provider_details.client_id 
-            and client_secret
-            and access_token_details.get("refresh_token")
-            and provider
-            ):
-            send_logout_request(provider_details.base_url, 
-                                provider_details.logout_url, 
-                                provider_details.client_id, 
-                                client_secret, 
-                                access_token_details.get("refresh_token"), 
-                                provider)
+    base_url = frappe.get_value("Social Login Key", provider, "base_url")
+    encoded_string = frappe.request.cookies.get(provider)
+    decoded_string = urllib.parse.unquote(encoded_string.strip('%22'))
+    params = urllib.parse.parse_qs(decoded_string)
+    session_state = params.get('session_state', [None])[0]
+    if base_url and session_state:
+        delete_keycloak_session(base_url, session_state, provider)
+
+def delete_keycloak_session(base_url, session_state, provider):
+    base_url = base_url.replace("realms", "admin/realms")
+    url = f"{base_url}sessions/{session_state}"
+    access_token = get_access_token()
+    headers = {
+    'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.delete(url, headers=headers)
+    if response.status_code == 204:
+        frappe.msgprint(f"Logout successfully from {frappe.get_value('Social Login Key', provider, 'provider_name')}")
+    else:
+        frappe.msgprint(f"Logout from {frappe.get_value('Social Login Key', provider, 'provider_name')} failed")
 
 def send_logout_request(base_url, logout_endpoint, client_id, client_secret, refresh_token, provider):
     url = f"{base_url}{logout_endpoint}"
