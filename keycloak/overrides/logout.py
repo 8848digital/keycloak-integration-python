e195116bed88f364a2a5388eb74b4139e76313e0
Touhid Mullick <touhid@8848digital.com>
2024-09-25 21:09:45 +0530

    feat:error log sso logout fail

--- keycloak/overrides/logout.py
+++ keycloak/overrides/logout.py
@@ -3,6 +3,7 @@ import requests
 import json
 import urllib.parse
 from keycloak.utils.utils import get_access_token
+from urllib.parse import urljoin
 
 def logout():
     provider = frappe.request.cookies.get('provider')
@@ -22,13 +23,23 @@ def delete_keycloak_session(base_url, session_state, provider):
     base_url = base_url.replace("realms", "admin/realms")
     url = f"{base_url}sessions/{session_state}"
     access_token = get_access_token()
+    if not base_url.endswith('/'):
+        base_url += '/'
+    url = urljoin(base_url, f"sessions/{session_state}")
     headers = {
     'Authorization': f'Bearer {access_token}'
     }
     response = requests.delete(url, headers=headers)
     if response.status_code == 204:
+        error_log = {
+        "response": response.text,
+        "base_url": base_url,
+        "session_state": session_state,
+        "access_token": access_token
+        }
         frappe.msgprint(f"Logout successfully from {frappe.get_value('Social Login Key', provider, 'provider_name')}")
     else:
+        frappe.log_error("SSO Logout Error", error_log)
         frappe.msgprint(f"Logout from {frappe.get_value('Social Login Key', provider, 'provider_name')} failed")
 
 def send_logout_request(base_url, logout_endpoint, client_id, client_secret, refresh_token, provider):
